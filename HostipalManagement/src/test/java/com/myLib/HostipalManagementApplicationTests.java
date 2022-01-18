be408e186a879e5ed2e0b2a1048cd4af22b69949
moham <moham@DESKTOP-ID0I752>
2022-01-18 14:20:06 +0530

    Fisrt Commit

---
+++ HostipalManagement/src/test/java/com/myLib/HostipalManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.util.Assert;
+
+import com.myLib.model.*;
+import com.myLib.service.*;
+import org.junit.Assert;
+import org.junit.Before;
+
+@SpringBootTest
+class HostipalManagementApplicationTests {
+	
+
+}
